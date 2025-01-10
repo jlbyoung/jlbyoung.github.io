a69954b6cf252acb66a3d2a0940da2d9c2e51bbf
James Young <imjamesyoung@gmail.com>
2025-01-09 17:52:01 -0800

    more mailto

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import { Button } from "./ui/button";
 import { HeroParallax } from "./ui/hero-parallax";
+import { useRouter } from "next/router";
 
 export function HeroSection() {
+  const router = useRouter();
   const products = [
     {
       title: "PEAR Health Labs",
@@ -51,7 +53,10 @@ export function HeroSection() {
             </div>
           </h2>
           <div className="flex flex-col sm:flex-row justify-center gap-4 w-[60%] mx-auto py-2">
-            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <button
+              className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              onClick={() => router.push("mailto:imjamesyoung@gmail.com")}
+            >
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-16 py-5 text-sm font-medium text-white backdrop-blur-3xl">
                 Let's Build
