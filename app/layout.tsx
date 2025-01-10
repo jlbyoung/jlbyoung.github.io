233c97fc6da1bf8f42bf883ebe5bd1169c78a3b5
James Young <imjamesyoung@gmail.com>
2025-01-09 18:07:37 -0800

    fav & title

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>James Young Portfolio</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
